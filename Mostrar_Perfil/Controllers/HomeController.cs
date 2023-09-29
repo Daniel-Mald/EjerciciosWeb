a4ed0261183eeff7d52e16d89b679b183cbe64e0
Carlos Daniel Maldonado Muñoz <201G0266@rcarbonifera.tecnm.mx>
2023-09-29 06:24:48 -0600

    Agregar archivos de proyecto.

---
+++ Mostrar_Perfil/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mostrar_Perfil.Models;
+
+namespace Mostrar_Perfil.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+          
+            return View();
+        }
+        public ActionResult MiPerfil()
+        {
+            MiPerfilModel vm = new();
+            return View(vm);
+        }
+
+        
+    }
+}
---
+++ Mostrar_Perfil/Models/MiPerfilModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Mostrar_Perfil.Models
+{
+    public class MiPerfilModel
+    {
+        public string Nombre { get; set; } = "Carlos Daniel Maldonado Muñoz";
+        public string Semestre { get; set; } = "7";
+        public string NumeroControl { get; set; } = "201G0266";
+        public string Estado { get; set; } = "Coahuila";
+        public int Edad { get; set; } = 23;
+        public string Carrera { get; set; } = "Ingenieria en sistemas computacionales";
+    }
+}
