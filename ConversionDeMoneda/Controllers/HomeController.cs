36b5d0b7fef4ea000f985acc8151f50699427f2b
Carlos Daniel Maldonado Muñoz <201G0266@rcarbonifera.tecnm.mx>
2023-10-01 20:40:29 -0600

    Conversor de monedas

---
+++ ConversionDeMoneda/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using ConversionDeMoneda.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConversionDeMoneda.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[HttpPost]
+        public IActionResult Index(ConversionViewModel vm)
+        {
+            
+            if (vm.transaccion == "PesosADolares")
+            {
+                vm.cantidadDestino = (vm.cantidadOrigen / 18).ToString("c");
+                vm.tipoMonedaOrigen = "pesos";
+                vm.tipoMonedaDestino = "dolares";
+            }
+            else
+            {
+                vm.cantidadDestino = (vm.cantidadOrigen * 18).ToString("c");
+                vm.tipoMonedaOrigen = "dolares";
+                vm.tipoMonedaDestino = "pesos";
+            }
+                return View(vm);
+        }
+        
+    }
+}
---
+++ ConversionDeMoneda/Models/Entities/ConversionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace ConversionDeMoneda.Models.Entities
+{
+    public class ConversionViewModel
+    {
+        public decimal cantidadOrigen { get; set; }
+        public string tipoMonedaOrigen { get; set; } = "pesos";
+        public string cantidadDestino { get; set; } = "";
+        public string tipoMonedaDestino { get; set; } = "dolares";
+        public string transaccion { get; set; } = "PesosADolares";
+
+       
+    }
+}
